f76fabf80d578258d1af3baeb78e03a49a2b84d7
nazimudheen <nazimudheen267@gmail.com>
2021-10-17 00:49:29 +0530

    navbar functionality added

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,21 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['src/scripts/Header.js', 'src/Homepage.js'],
+        dest: 'build/js/Home.js',
+      },
+    },
+    uglify: {
+      my_target: {
+        files: { 'build/js/Home.js': ['build/js/Home.js'] },
+      },
+    },
+  });
 
-module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.initConfig({
-		concat:{
-			css:{
-				src: ['src/one.css','src/two.css'],
-				dest: 'build/css/style.css'
-			}
-		},
-		uglify:{
-			my_target:{
-				files:{
-					'build/js/all.js' : ['src/home.js']
-			}
-			}
-			}
-	})
-
-	
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-
-	grunt.registerTask('concat-js',['concat:css']);
-	grunt.registerTask('default',['uglify:my_target'])
-}
+  grunt.registerTask('concat-js', ['concat:css']);
+  grunt.registerTask('default', ['uglify:my_target']);
+};
---
+++ src/scripts/Header.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+// dom variables for header
+  const bodyCls = document.getElementById('js-body').classList;
+  const overlay = document.getElementById('js-overlay');
+  const cartBtn = document.getElementById('js-cart');
+  const cartBox = document.getElementById('js-cart-detail');
+  const close = document.getElementById('js-close');
+  const sideBar = document.getElementById('js-nav-side');
+  const menuBtn = document.getElementById('js-menu-bar');
+
+  // re usable functions for overlay
+  function bgOn() {
+    overlay.hidden = false;
+    bodyCls.add('no-scroll');
+  }
+
+  function bgOff() {
+    overlay.hidden = true;
+    bodyCls.remove('no-scroll');
+  }
+
+  // button event for cart-list
+  cartBtn.addEventListener('click', () => {
+    if (overlay.hidden && cartBox) {
+      bgOn();
+      cartBox.hidden = false;
+    } else {
+      bgOff();
+      cartBox.hidden = true;
+    }
+  });
+
+  menuBtn.addEventListener('click', () => {
+    sideBar.classList.add('side-in');
+    sideBar.classList.remove('side-out');
+    overlay.style.zIndex = 5;
+    bgOn();
+  });
+
+  close.addEventListener('click', () => {
+    sideBar.classList.add('side-out');
+    sideBar.classList.remove('side-in');
+    overlay.style.zIndex = 1;
+    bgOff();
+  });
+};
